14dfe99c157075fddb7e192e90d055ec3e6a38cb
Lucasmaiaesilva <lucas.maia.ext@evidentscientific.com>
2026-03-27 16:20:03 -0300

    feat: swetrix

---
+++ src/env.d.ts
@@ -0,0 +1,8 @@
+interface ImportMetaEnv {
+  readonly VITE_SWETRIX_PROJECT_ID: string
+  readonly VITE_SWETRIX_API_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import * as Swetrix from 'swetrix'
+
+const projectId = import.meta.env.VITE_SWETRIX_PROJECT_ID
+const apiURL = import.meta.env.VITE_SWETRIX_API_URL
+
+if (projectId) {
+  Swetrix.init(projectId, {
+    apiURL: apiURL,
+  })
+  Swetrix.trackViews()
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
